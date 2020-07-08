7035a5f077ca4a5cd23c6597203212f731d6cfda
Yu Li <lyo.gavin@gmail.com>
2020-07-08 16:58:30 -0700

    debug run script

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-nohup python3 ./imagenet.py --pretrained --data /media/windows_4/kinetics/compress --lr 0.05 --arch mobilenetv2 \
+#for debug
+nohup python3 ./imagenet.py --pretrained --data /home/pi/work/debug_kinetics/ --lr 0.05 --arch mobilenetv2 \
   --workers 8 --epochs 150 --weight-decay 4e-5 --print-freq 1 --lr-decay cos --width-mult 1.0 --input-size 224 \
   --weight /home/pi/work/mobilenetv2.pytorch/pretrained/mobilenetv2_1.0-0c6065bc.pth &
+
+#nohup python3 ./imagenet.py --pretrained --data /media/windows_4/kinetics/compress --lr 0.05 --arch mobilenetv2 \
+#  --workers 8 --epochs 150 --weight-decay 4e-5 --print-freq 1 --lr-decay cos --width-mult 1.0 --input-size 224 \
+#  --weight /home/pi/work/mobilenetv2.pytorch/pretrained/mobilenetv2_1.0-0c6065bc.pth &
