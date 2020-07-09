074c406728476fb00c9de2bfc31e01a269e9b52e
Yu Li <lyo.gavin@gmail.com>
2020-07-09 08:20:19 -0700

    logs

--- models/imagenet/mobilenetv2.py
+++ models/imagenet/mobilenetv2.py
@@ -136,14 +136,18 @@ class MobileNetV2(nn.Module):
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
+                print("_initialize_weights Conv2d...")
+
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
+                print("_initialize_weights BatchNorm2d...")
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
+                print("_initialize_weights Linear...")
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
