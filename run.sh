f43fc51e4d251986a24885ec7da86e756c7a4979
Yu Li <lyo.gavin@gmail.com>
2020-07-09 10:48:42 -0700

    reduce batch size to reduce OOM

--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@
 #  --weight /home/pi/work/mobilenetv2.pytorch/pretrained/mobilenetv2_1.0-0c6065bc.pth &
 
 nohup python3 ./imagenet.py --pretrained --data /media/windows_4/kinetics/compress --lr 0.05 --arch mobilenetv2 \
+  --batch-size 32 \
   --workers 8 --epochs 150 --weight-decay 4e-5 --print-freq 1 --lr-decay cos --width-mult 1.0 --input-size 224 \
   --weight /home/pi/work/mobilenetv2.pytorch/pretrained/mobilenetv2_1.0-0c6065bc.pth &
